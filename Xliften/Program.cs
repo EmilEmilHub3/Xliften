e1183631ad3020aa104af97a2c7e1634d5bd90f6
EmilEmilHub2 <10464750@ucn.dk>
2025-11-06 16:24:49 +0100

    Endpoint med GetVideoByIdAsync(id) og GetAllVideosAsync

--- Xliften/Program.cs
+++ Xliften/Program.cs
@@ -1,4 +1,6 @@
 
+using Xliften.Services.ServiceInterfaces;
+
 namespace Xliften
 {
     public class Program
@@ -7,44 +9,14 @@ namespace Xliften
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddControllers();
+            builder.Services.AddSingleton<GridFsVideoService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
+            app.MapControllers();
             app.Run();
+
         }
     }
 }
